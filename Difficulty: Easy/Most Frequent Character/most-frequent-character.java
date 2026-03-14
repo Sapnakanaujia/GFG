b73563f497ef6fc1b8f76ddc16614796e8a6a7e4
Sapnakanaujia <sapnakanaujia37@gmail.com>
2026-03-14 17:24:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Most Frequent Character/most-frequent-character.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char getMaxOccuringChar(String s) {
+        // HashMap<Character,Character> map = new HasahMap<>();
+        int[] freq= new int[26]; 
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        int max=0;
+        char ans='a';
+        for(int i=0;i<26;i++){
+            if(freq[i]>max){
+                max= freq[i];
+                ans=(char)(i+'a');
+            }
+        }
+        return ans;
+    }
+}
