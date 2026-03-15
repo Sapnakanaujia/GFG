4ed4204144d144097aaf633f8ccaacac98028046
Sapnakanaujia <sapnakanaujia37@gmail.com>
2026-03-15 19:05:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.java
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.java
@@ -1,23 +1,27 @@
+import java.util.*;
 class Solution {
-    public int circularSubarraySum(int arr[]) {
-        int n= arr.length;
-        int currMaxSum=0,currMinSum=0;
-        int totalSum=0;
-        int maxSum=arr[0],minSum=arr[0];
-        for(int i=0;i<n;i++){
-            currMaxSum=Math.max(currMaxSum+arr[i],arr[i]);
-            maxSum=Math.max(currMaxSum,maxSum);
-            currMinSum=Math.min(currMinSum+arr[i],arr[i]);
-            minSum=Math.min(currMinSum,minSum);
-            
-            totalSum+=arr[i];
-        }
-        int normalSum=maxSum;
-        int circularSum=totalSum-minSum;
-        if(minSum==totalSum){
-            return normalSum;
-        }
-        return Math.max(normalSum,circularSum);
+    public int maxCircularSum(int arr[]) {
+        int n=arr.length;
+        int total= 0;
+        for(int num:arr)total+=num;
+        int maxPrefix= arr[0];
+        int maxSubSum = maxPrefix;
+        int minPrefix= arr[0];
+        int minSubSum = minPrefix;
         
+        for(int i=1;i<n;i++){
+            maxPrefix= Math.max(maxPrefix+arr[i], arr[i]);
+            maxSubSum= Math.max(maxSubSum, maxPrefix);
+            
+            minPrefix= Math.min(minPrefix+arr[i], arr[i]);
+            minSubSum= Math.min(minSubSum, minPrefix);
+            
+        }
+        int ans1= maxSubSum;
+        int ans2= total- minSubSum;
+        if(ans2==0){
+            return ans1;
+        }
+        return Math.max(ans1, ans2);
     }
 }
